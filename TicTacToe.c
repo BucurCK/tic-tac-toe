04e1c3fdd5149d6809cd1848d99dfca9fca2810e
BucurCK <bucur.alexflr@gmail.com>
2022-07-26 21:48:02 +0300

    Made functions, added Tie condition, made it so you can't rewrite values, made it a ARRAY (was a matrix), and fixed A LOT of BUGS

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,111 +1,170 @@
 /**
- * TODO: Create functions cause this code is UGLY
- * TODO: Make it impossible to rewrite symbols in the game
+ * // TODO: Create functions cause this code is UGLY
+ * // TODO: Make it impossible to rewrite symbols in the game
+ * // TODO: Add TIE state
  * * Maybe just make the choice of symbols (x/o) automatic
  * ! Add an AI(or something like that) so you can play solo - you sad FUCK
- * * Try to make some kind GUI for this(don't know how atm) 
+ * * Try to make some kind GUI for this(don't know how atm)
  * * Write the functions in different files(just to learn it -- for this kind of short code it's pretty useless)
+ * // MAKE IT A VECTOR YOU STUPID FUCK -- actually just leave it :)
  */
+
+void printTicToc(char *game);
+void fillUp(char *game);
+int getNum(char x[],char cond);
+int checkWin(char *game);
+int checkRewrite(int i, int memory[], int *k);
 
 #include <stdio.h>
 
-#define SIZE 3
+#define SIZE 9
 
 char line[10];
 
 int main(){
-    char game[SIZE][SIZE];
-    int i, j, k = 49;
+    char game[SIZE];
+    int i;
     int test = 1; // keeps the while going till a win condition is met
-    int pos; //position form 1-9
-    char sym; //the symbol used in the game (will set in the future to only be x or o)
+    char *pgame;
+    pgame = game;
+    int memory[SIZE]; //remember where x/o have been placed
+    int k = 0; // keep count of how may x/o have been placed
+    int j;
 
-
-    for(i = 0; i < SIZE; i++){
-        for(j = 0; j < SIZE; j++){
-            game[i][j] = k;
-            k++;
-        }
-    }
-    /*Fill up the matrix with ASCII codes for 1-9 (starts from '49' = 1)*/
-    
-    printf("    | %c | %c | %c |\n",game[0][0], game[0][1], game[0][2]);
-    printf("    | %c | %c | %c |\n",game[1][0], game[1][1], game[1][2]);
-    printf("    | %c | %c | %c |\n",game[2][0], game[2][1], game[2][2]);
-    printf("\n");
-    /*Print the initial state of the game (filled with 1-9)*/
+    fillUp(pgame);
+    printTicToc(pgame);
 
     printf("Instruction: Input the line number and right next to it the symbol you wish to play with(use x and o pls):\n");
 
     while(test == 1){
 
         resetPosition:
-        printf("Input line(1-9): ");
-        fgets(line, sizeof(line), stdin);
-        sscanf(line, "%d", &pos);
-        if(pos < 1 || pos > 9){
-            printf("WRONG INPUT, please use numbers from 1 to 9\n");
-            goto resetPosition;
+        i = getNum("Input line(1-9): ", 'd') - 1;
+        if(i < 0 || i > 8){
+        printf("WRONG INPUT, please use numbers from 1 to 9\n");
+        goto resetPosition;
         }
-        i = (pos-1) / 3;
-        j = (pos-1) % 3;
-        /*Gets the position int then checks it to be between 1-9 
-        (if not it will scan it again -- goto -- ) and than it 
-        assigns that to a position in the matrix(by dividing and 
-        assigning the whole part to i and fractional to j -- this 
-        will always give the correct position in a 3by3 matrix*/
-
+        i = checkRewrite(i, memory, &k);
+        if(i == -1){
+        printf("THE BOX IS ALREADY FILLED\n");
+        goto resetPosition;
+        }
+        /*Gets the position(1-9) -- turns it in a array position (0-8)
+        (if it isn't 0-8 it will scan it again -- goto -- )
+        -- assigns it to the array position*/
+       
         resetSymbol:
-        printf("Input symbol: ");
-        fgets(line, sizeof(line), stdin);
-        sscanf(line, "%c", &sym);
-        if(sym != 'x' && sym != 'o'){
+        game[i] = getNum("Input symbol: ", 'c');
+        if(game[i] != 'x' && game[i] != 'o'){
             printf("WRONG INPUT, please use x or o\n");
             goto resetSymbol;
         }
-        game[i][j] = sym;
-        /*Gets the symbol char then checks it to be either x or o;
-        if not it will scan it again and than assign it to a 
-        position in the matrix*/
+        /*Gets the symbol then checks it to be either x or o;
+        if not it will scan it again and than assign it
+        to 'i' position in the char*/
 
-        for(i = 0; i < SIZE && test == 1; i++){
-            if((game[0][i] == game[1][i]) && (game[2][i] == game[1][i])){
-                printf("......YOU WON......\n");
-                test = 0;
-                break;
-            }
-            else if((game[i][0] == game[i][1]) && (game[i][2] == game[i][1])){
-                printf("......YOU WON......\n");
-                test = 0;
-                break;
-            }
-            else if((game[0][0] == game[1][1]) && (game[2][2] == game[1][1])){
-                printf("......YOU WON......\n");
-                test = 0;
-                break;
-            }
-            else if((game[0][2] == game[1][1]) && (game[2][0] ==  game[1][1])){
-                printf("......YOU WON......\n");
-                test = 0;
-                break;
-            }
+        test = checkWin(pgame);
+        if( k == 9){
+            test = 1;
+            printf("........TIE........\n");
+         }         
+       
+        printTicToc(pgame);
 
-        }
-        /*it will check for a win condition by checking every line across, 
-        then downward, then both diagonals*/
-
-        //TODO: the diagonals check should be moved outside the for, because it shouldn't be checked 3 times :) )
-        
-    printf("   | %c | %c | %c |\n",game[0][0], game[0][1], game[0][2]);
-    printf("   | %c | %c | %c |\n",game[1][0], game[1][1], game[1][2]);
-    printf("   | %c | %c | %c |\n",game[2][0], game[2][1], game[2][2]);
-    printf("\n");
-
-    /*Prints the current state of the game*/
-
-    }    
+    }   
 
     return 0;
 }
 
+void printTicToc(char *game){
+   
+    printf("   | %c | %c | %c |\n",*(game + 0), *(game + 1), *(game + 2));
+    printf("   | %c | %c | %c |\n",*(game + 3), *(game + 4), *(game + 5));
+    printf("   | %c | %c | %c |\n",*(game + 6), *(game + 7), *(game + 8));
+    printf("\n");
 
+}
+    /*Print the state of the game*/
+
+void fillUp(char *game){
+    int i;
+    int k = 49;
+
+    for(i = 0; i < SIZE; i++){
+        *(game + i) = k;
+        k++;
+    }
+}
+    /*Fill up the matrix with ASCII codes for 1-9 (starts from '49' = 1)*/
+
+int getNum(char a[], char cond){
+    int x;
+    char y;
+
+    printf("%s", a);
+    switch (cond)
+    {
+    case ('d'):
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%d", &x);
+            return x;
+        break;
+    case ('c'):
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%c", &y);
+            return y;
+        break;
+    default:
+        return x;
+        break;
+    }
+}
+
+    /*Gets the input from user -- also checks it to be either int or char*/
+
+int checkWin(char *game){
+    int i;
+    int j;
+    int test = 1;
+
+    if((*(game + 0) == *(game + 4)) && (*(game + 0) == *(game + 8))){
+                printf("......YOU WON......\n");
+                test = 0;
+    }
+    else if((*(game + 2) == *(game + 4)) && (*(game + 2) ==  *(game + 6))){
+                printf("......YOU WON......\n");
+                test = 0;
+
+    }
+   
+    for(i = 0, j = 0; i < 3 && test == 1; i++, j += 2){
+            if((*(game + i + j) == *(game + i + j + 1)) && (*(game + i + j) == *(game + i + j + 2))){
+                printf("......YOU WON......\n");
+                test = 0;
+                break;
+            }
+            else if((*(game + i) == *(game + i + 3)) && (*(game + i) == *(game + i + 6))){
+                printf("......YOU WON......\n");
+                test = 0;
+                break;
+            }      
+        }
+
+        return test;
+}
+       
+        /*checks for a win condition by comparing every
+        line across, then downward, then both diagonals
+        and return 0 for win and 1 for lose*/
+
+int checkRewrite(int i, int memory[], int *k){
+    int x;
+    for(x = 0; x < *k; x++){
+        if( i == *(memory + x)){
+            return -1;
+        }
+    }
+    *(memory + *k) = i;
+    *k = *k + 1;
+    return i;
+}
