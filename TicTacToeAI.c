23245a30278d184c7702a60c784ac839fd8b1f4c
BucurCK <bucur.alexflr@gmail.com>
2023-05-21 14:54:28 +0300

    Added confirmation to exet executable

--- TicTacToeAI.c
+++ TicTacToeAI.c
@@ -2,7 +2,7 @@
  * // TODO: Create functions cause this code is UGLY
  * // TODO: Make it impossible to rewrite symbols in the game
  * // TODO: Add TIE state
- * /////* Maybe just make the choice of symbols (x/o) automatic
+ * /////* Maybe just make the choice of symbols (X/O) automatic
  * //! Add an AI(or something like that) so you can play solo - you sad FUCK
  * //! REMOVE goto cause its stupid
  * ! Rename the variables
@@ -13,7 +13,7 @@
 
 void printTicToc(char *game);
 void fillUp(char *game);
-int getNum(char x[],char cond);
+int getNum(char X[],char cond);
 int checkWin(char *game, int turn);
 int checkRewrite(int i, int memory[], int turn);
 void artificialIntelligence(char *game, char symbol_AI,char symbol_User, int memory[], int turn);
@@ -28,27 +28,27 @@ void artificialIntelligence(char *game, char symbol_AI,char symbol_User, int mem
 char line[10];
 
 int main(){
-    char game[SIZE]; // the x&o array
+    char game[SIZE]; // the X&O array
     int i; // array position
     int test = 1; // keeps the while going till a win condition is met
     char *pgame = game;
-    int memory[SIZE]; //remember where x/o have been placed
-    int turn = 0; // keep count of how may x/o have been placed and how many turns have passed
+    int memory[SIZE]; //remember where X/O have been placed
+    int turn = 0; // keep count of how may X/O have been placed and how many turns have passed
     int checker;
 
     fillUp(pgame);
     printTicToc(pgame);
 
-    printf("Instruction: Input the line number and right next to it the symbol_AI you wish to play with(use x and o pls):\n");
+    printf("Instruction: Input the line number and right next to it the symbol_AI you wish to play with(use X and O pls):\n");
 
     while(test == 1){
 
         if(turn % 2 == 0){
-            printf("TURN: %d -- x\n", turn + 1);
+            printf("TURN: %d -- X\n", turn + 1);
         }else{
-            printf("TURN: %d -- o\n", turn + 1);
+            printf("TURN: %d -- O\n", turn + 1);
             
-            artificialIntelligence(pgame, 'o', 'x', memory, turn);
+            artificialIntelligence(pgame, 'O', 'X', memory, turn);
             test = checkWin(pgame, turn);
             printTicToc(pgame);
             if(test != 1){
@@ -66,7 +66,7 @@ int main(){
             i = getNum("Input line(1-9): ", 'd') - 1;
         }
         /*Gets the position(1-9) -- turns it in a array position (0-8)
-        (if it isn't o-8 it will scan it again*/
+        (if it isn't O-8 it will scan it again*/
 
         i = checkRewrite(i, memory, turn);
         while(i == -1){
@@ -82,9 +82,9 @@ int main(){
         -- re-scans it and rechecks it to be between 1-9*/
         //printf("### turn=%d\n",turn);
         
-        game[i] = 'x'; // puts the symbol
+        game[i] = 'X'; // puts the symbol
         
-        /*Even turns are 'o'; uneven turns are 'x'*/
+        /*Even turns are 'O'; uneven turns are 'X'*/
 
         test = checkWin(pgame, turn);
         turn++;
@@ -99,7 +99,13 @@ int main(){
        
         printTicToc(pgame);
 
-    }   
+    }
+    #ifdef _WIN32
+        system("pause");
+    #else
+        printf("Press enter to exit...");
+        getchar();
+    #endif  
 
     return 0;
 }
@@ -126,7 +132,7 @@ void fillUp(char *game){
     /*Fill up the matrix with ASCII codes for 1-9 (starts from '49' = 1)*/
 
 int getNum(char a[], char cond){
-    int x;
+    int X;
     char y;
 
     printf("%s", a);
@@ -134,8 +140,8 @@ int getNum(char a[], char cond){
     {
     case ('d'):
         fgets(line, sizeof(line), stdin);
-        sscanf(line, "%d", &x);
-            return x;
+        sscanf(line, "%d", &X);
+            return X;
         break;
     case ('c'):
         fgets(line, sizeof(line), stdin);
@@ -143,7 +149,7 @@ int getNum(char a[], char cond){
             return y;
         break;
     default:
-        return x;
+        return X;
         break;
     }
 }
@@ -157,9 +163,9 @@ int checkWin(char *game, int turn){
     char symbol_WIN;
 
     if(turn % 2 == 0){
-        symbol_WIN = 'x';
+        symbol_WIN = 'X';
     }else{
-        symbol_WIN = 'o';
+        symbol_WIN = 'O';
     }
 
     if((*(game + 0) == *(game + 4)) && (*(game + 0) == *(game + 8))){
@@ -193,9 +199,9 @@ int checkWin(char *game, int turn){
         and return 0 for win and 1 for lose*/
 
 int checkRewrite(int i, int memory[], int turn){
-    int x;
-    for(x = 0; x < turn; x++){
-        if( i == *(memory + x)){
+    int X;
+    for(X = 0; X < turn; X++){
+        if( i == *(memory + X)){
             return -1;
         }
     }
@@ -216,17 +222,14 @@ void artificialIntelligence(char *game, char symbol_AI,char symbol_User, int mem
         if(*(game + 0) == *(game + 4) && *(game + 8) != symbol_AI && *(game + 8) != symbol_User){
             *(game + 8) = symbol_AI;
             *(memory + turn) = 8;
-            //printf("########TESTING1########\n");
             break;
         }else if(*(game + 0) == *(game + 8) && *(game + 4) != symbol_AI && *(game + 4) != symbol_User){
             *(game + 4) = symbol_AI;
             *(memory + turn) = 4;
-            //printf("########TESTING2########\n");
             break;
         }else if(*(game + 4) == *(game + 8) && *(game + 0) != symbol_AI && *(game + 0) != symbol_User){
             *(game + 0) = symbol_AI;
             *(memory + turn) = 0;
-            //printf("########TESTING3########\n");
             break;
         }
         /*first diagonal*/
@@ -234,17 +237,14 @@ void artificialIntelligence(char *game, char symbol_AI,char symbol_User, int mem
         if(*(game + 2) == *(game + 4) && *(game + 6) != symbol_AI && *(game + 6) != symbol_User){
             *(game + 6) = symbol_AI;
             *(memory + turn) = 6;
-            //printf("########TESTING4########\n");
             break;
         }else if(*(game + 2) == *(game + 6) && *(game + 4) != symbol_AI && *(game + 4) != symbol_User){
             *(game + 4) = symbol_AI;
             *(memory + turn) = 4;
-            //printf("########TESTING5########\n");
             break;
         }else if(*(game + 4) == *(game + 6) && *(game + 2) != symbol_AI && *(game + 2) != symbol_User){
             *(game + 2) = symbol_AI;
             *(memory + turn) = 2;
-            //printf("########TESTING6########\n");
             break;
         }
         /*second diagonal*/
@@ -253,39 +253,33 @@ void artificialIntelligence(char *game, char symbol_AI,char symbol_User, int mem
             if(*(game + i + j) == *(game + i + j + 1) && *(game + i + j + 2) != symbol_AI && *(game + i + j + 2) != symbol_User){
                 *(game + i + j + 2) = symbol_AI;
                 *(memory + turn) =( i + j + 2);
-                //printf("########TESTING7########\n");
                 test++;
                 break;
             }else if(*(game + i + j) == *(game + i + j + 2) && *(game + i + j + 1) != symbol_AI && *(game + i + j + 1) != symbol_User){
                 *(game + i + j + 1) = symbol_AI;
                 *(memory + turn) = (i + j + 1);
                 test++;
-                //printf("########TESTING8########\n");
                 break;
             }else if(*(game + i + j + 1) == *(game + i + j + 2) && *(game + i + j) != symbol_AI && *(game + i + j) != symbol_User){
                 *(game + i + j) = symbol_AI;
                 *(memory + turn) = (i + j);
                 test++;
-                //printf("########TESTING9########\n");
                 break;
             }
         /* across */
             if(*(game + i) == *(game + i + 3) && *(game + i + 6) != symbol_AI && *(game + i + 6) != symbol_User){
                 *(game + i + 6) = symbol_AI;
                 *(memory + turn) = (i + 6);
-                //printf("########TESTING10########\n");
                 test++;
                 break;
             }else if(*(game + i) == *(game + i + 6) && *(game + i + 3) != symbol_AI && *(game + i + 3) != symbol_User){
                 *(game + i + 3) = symbol_AI;
                 *(memory + turn) = (i + 3);
-                //printf("########TESTING11########\n");
                 test++;
                 break;
             }else if(*(game + i + 3) == *(game + i + 6) && *(game + i) != symbol_AI && *(game + i) != symbol_User){
                 *(game + i) = symbol_AI;
                 *(memory + turn) = i;
-                //printf("########TESTING12########\n");
                 test++;
                 break;
             }
